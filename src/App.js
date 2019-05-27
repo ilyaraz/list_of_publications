010c49b32d23b3f234713a30ebb50addceaed0d9
Ilya Razenshteyn <ilyaraz@mit.edu>
2019-05-27 12:14:10 -0500

    small fixes

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import Papers from './Papers'
 import data from './pubs.json'
 import Menu from './Menu'
 import { Container } from 'react-bootstrap'
-
+import MathJax from 'react-mathjax2'
 
 class App extends React.Component {
 
@@ -23,9 +23,29 @@ class App extends React.Component {
   render() {
     return (<div className="App">
       <Menu selected={this.state.selected} setSelected={this.setSelected} />
-      <Container>
-        <Papers selected={this.state.selected} papers={this.state.papers} />
-      </Container>
+      <MathJax.Context
+        input='ascii'
+        onLoad={() => console.log("Loaded MathJax script!")}
+        onError={(MathJax, error) => {
+          console.warn(error);
+          console.log("Encountered a MathJax error, re-attempting a typeset!");
+          MathJax.Hub.Queue(
+            MathJax.Hub.Typeset()
+          );
+        }}
+        script="https://cdnjs.cloudflare.com/ajax/libs/mathjax/2.7.2/MathJax.js?config=AM_HTMLorMML"
+        options={{
+          asciimath2jax: {
+            useMathMLspacing: true,
+            delimiters: [["$", "$"]],
+            preview: "none",
+          }
+        }}
+      >
+        <Container>
+          <Papers selected={this.state.selected} papers={this.state.papers} />
+        </Container>
+      </MathJax.Context>
     </div>)
   }
 }
